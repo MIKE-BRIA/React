125a21c696e56f0224c61501d57df5d3db3398e2
Brian Michael <brianmichaeladero@gmail.com>
2024-03-24 14:44:58 +0300

    new feature

--- ReactEssentials/src/App.jsx
+++ ReactEssentials/src/App.jsx
@@ -1,17 +1,39 @@
+//*importing images
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header></Header>
       <main>
         <h2>Time to get started!</h2>
       </main>
+      <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts {...CORE_CONCEPTS[0]}></CoreConcepts>
+          <CoreConcepts {...CORE_CONCEPTS[1]}></CoreConcepts>
+          <CoreConcepts {...CORE_CONCEPTS[2]}></CoreConcepts>
+          <CoreConcepts
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+          ></CoreConcepts>
+        </ul>
+      </section>
+
+      <section id="examples">
+        <h2>Examples</h2>
+        <menu>
+          <TabButton>components</TabButton>
+          <TabButton>JSX</TabButton>
+          <TabButton>Props</TabButton>
+          <TabButton>State</TabButton>
+        </menu>
+      </section>
     </div>
   );
 }
