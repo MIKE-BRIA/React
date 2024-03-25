34f98c646d2c0d79ea59488af541aaff45a1dd88
Brian Michael <brianmichaeladero@gmail.com>
2024-03-25 14:59:20 +0300

    updated

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-  
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    // setActivePlayer((activePlayer) => !activePlayer);
+    setActivePlayer((activePlayer) => (activePlayer === "X" ? "O" : "X"));
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ TicTacToe/src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleInputChange(e) {
+    console.log(e);
+    setPlayerName(e.target.value);
+  }
+
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            value={playerName}
+            required
+            onChange={handleInputChange}
+          ></input>
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
+}
