e7e1f2c967ca20cd65e733d8a127ba271ab8bef8
Brian Michael <brianmichaeladero@gmail.com>
2024-03-27 06:09:30 +0300

    app

--- InvestCalc/src/App.jsx
+++ InvestCalc/src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import UserAdd from "./components/UserAdd";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000000,
+    annualInvestment: 10000,
+    expectedReturn: 5,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
     <>
-      <UserAdd />
+      <UserAdd onChange={handleChange} userInput={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than zero</p>
+      )}
     </>
   );
 }
