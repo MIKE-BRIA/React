e3810242cd012a0d35344dfdcaf6a71d5675e533
Brian Michael <brianmichaeladero@gmail.com>
2024-04-04 09:44:40 +0300

    updated

---
+++ QuizApp/src/components/Question.jsx
@@ -0,0 +1,24 @@
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers";
+
+export default function Question({
+  questionText,
+  answers,
+  onSelectAnswer,
+  selectedAnswer,
+  answerState,
+  onSkipAnswer,
+}) {
+  return (
+    <div id="question">
+      <p>{questionText}</p>
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <Answers
+        answers={answers}
+        selectedAnswer={selectedAnswer}
+        answerState={answerState}
+        onSelect={onSelectAnswer}
+      />
+    </div>
+  );
+}
