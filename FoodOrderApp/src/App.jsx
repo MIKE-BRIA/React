24f465c635c16fe49f017521f800fce9168b82ba
Brian Michael <brianmichaeladero@gmail.com>
2024-04-08 13:25:46 +0300

    main point

--- FoodOrderApp/src/App.jsx
+++ FoodOrderApp/src/App.jsx
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from "./store/CartContex";
+import { UserProgressContextProvider } from "./store/UserProgressContex";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <CartContextProvider>
-      <Header />
-      <Meals />
+      <UserProgressContextProvider>
+        <Header />
+        <Cart />
+        <Meals />
+      </UserProgressContextProvider>
     </CartContextProvider>
   );
 }
