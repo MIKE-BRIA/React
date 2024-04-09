14bf12945697acf5e38b1731b14959f6ff1de260
Brian Michael <brianmichaeladero@gmail.com>
2024-04-09 12:57:38 +0300

    store

---
+++ ReactRedux/src/store/index.js
@@ -0,0 +1,52 @@
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterSlice from "./Redux/counter";
+import authSlice from "./Redux/auth";
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
+export default store;
+
+// //Reducer function
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "decrement") {
+//     return {
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
+
+//   if (action.type === "toggle") {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     };
+//   }
+
+//   return state;
+// };
+
+// //Store
+// const store = createStore(counterReducer);
+
+// export default store;
