b4f7e23251f426b2c023698b1dbe966f2bb370f3
Brian Michael <brianmichaeladero@gmail.com>
2024-04-17 08:07:42 +0300

    updated

---
+++ NextFoodApp/lib/meals.js
@@ -0,0 +1,12 @@
+import sql from "better-sqlite3";
+
+const db = sql("meals.db");
+
+export function getMeals() {
+  // throw new Error("Loading meals from database failed");
+  return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ? ").get(slug);
+}
