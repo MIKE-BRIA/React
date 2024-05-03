1f520de9ab83e64911cfa2c9edf62ee37e4b8e52
Brian Michael <brianmichaeladero@gmail.com>
2024-05-03 11:07:05 +0300

    updated

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn Drawing")];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GOODMORNING</h1>
+      <Todos items={todos} />
     </div>
   );
 }
