eab3f9bb9d00688f2c6d4cef2a86a0c5995deecf
Brian Michael <brianmichaeladero@gmail.com>
2024-05-14 15:28:58 +0300

    models

--- ecommerce/ecommerce/models/product.js
+++ ecommerce/ecommerce/models/product.js
@@ -1,9 +1,10 @@
-import { Schema, model, models } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 
 const ProductSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
+  category: { type: mongoose.Types.ObjectId, ref: "Category" },
   images: [{ type: String }],
 });
 
