b62676b8dbf18e5850dec93e484252891a529990
Brian Michael <brianmichaeladero@gmail.com>
2024-05-16 13:21:30 +0300

    components

--- ecommerce/ecommerce/components/Layout.jsx
+++ ecommerce/ecommerce/components/Layout.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Nav from "@/components/Nav";
+import Logo from "./Logo";
 
 const Layout = ({ children }) => {
+  const [showNav, setShowNav] = useState(false);
   const { data: session } = useSession();
 
   if (!session) {
     return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="bg-bgGray w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button
             onClick={() => signIn("google")}
@@ -21,10 +23,31 @@ const Layout = ({ children }) => {
   }
 
   return (
-    <div className="bg-blue-900 min-h-screen flex">
-      <Nav />
-      <div className="bg-white flex-grow my-2 mr-2 rounded-xl p-4">
-        {children}
+    <div className="bg-bgGray min-h-screen">
+      <div className="block md:hidden flex p-4">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+      <div className=" flex">
+        <Nav show={showNav} />
+        <div className=" flex-grow  h-screen  p-4">{children}</div>
       </div>
     </div>
   );
