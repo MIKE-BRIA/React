9f232222189ee91f828a9c3c8038c2e04d97bce5
Brian Michael <brianmichaeladero@gmail.com>
2024-05-17 21:21:36 +0300

    updated

---
+++ ecommerce/ecomclient/components/Featured.jsx
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import Center from "./Center";
+import Button from "./Button";
+
+const Bg = styled.div`
+  background-color: #222;
+  color: #fff;
+  padding: 50px 0;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-weight: normal;
+  font-size: 3rem;
+`;
+
+const Desc = styled.p`
+  color: #aaa;
+  font-size: 1.2rem;
+`;
+
+const Columnswrapper = styled.div`
+  display: grid;
+  grid-template-columns: 0.9fr 1.1fr;
+  gap: 40px;
+  img {
+    max-width: 100%;
+  }
+`;
+
+const Buttonwrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+`;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export default function Featured() {
+  return (
+    <Bg>
+      <Center>
+        <Columnswrapper>
+          <Column>
+            <div>
+              <Title>MacBook Pro </Title>
+              <Desc>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                excepturi recusandae cupiditate ratione facilis minima ab rerum
+                fugit aperiam soluta hic sunt officia, accusamus voluptate ut,
+                adipisci odit ad vel.
+              </Desc>
+              <Buttonwrapper>
+                <Button size="l" outline white>
+                  Read more
+                </Button>
+                <Button size="l" primary>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                  </svg>
+                  Add to Cart
+                </Button>
+              </Buttonwrapper>
+            </div>
+          </Column>
+
+          <Column>
+            <img
+              src="https://images.unsplash.com/photo-1523688867149-edc3047db358?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODJ8fG1hYyUyMGxhcHRvcCUyMHNjcmVlbnxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </Column>
+        </Columnswrapper>
+      </Center>
+    </Bg>
+  );
+}
